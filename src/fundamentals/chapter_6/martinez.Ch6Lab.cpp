ef7f13785fec69ab33510e57bed303b7d0a805bb
Nicholas Martinez <nedwardmartinez@gmail.com>
2023-02-04 15:44:58 -0600

    Prepare for fundamentals ch 6 lab

---
+++ src/fundamentals/chapter_6/martinez.Ch6Lab.cpp
@@ -0,0 +1,18 @@
+//******************************************************************
+// Programmer: Nicholas Martinez
+// Completed : 02//2023
+// Status    : 
+//
+// 
+//******************************************************************
+
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
