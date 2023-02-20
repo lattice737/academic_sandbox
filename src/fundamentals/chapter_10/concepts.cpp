9f84cc3b2daf509773a2b920f4411c0b97e7326a
Nicholas Martinez <nedwardmartinez@gmail.com>
2023-02-19 20:15:24 -0600

    Begin 1337 ch10 concepts

---
+++ src/fundamentals/chapter_10/concepts.cpp
@@ -0,0 +1,202 @@
+// Chapter 10 Concepts
+
+#include<iostream>
+#include<iomanip>
+#include<string>
+
+using namespace std;
+
+/*
+Vocabulary
+==========
+* Pointer - a value that represents the address of a memory location or holds the address of a variable
+* Address operator (&) - returns the memory address of its operand
+* Pointer variable - a variable that holds memory location addresses
+* Indirection operator (*) - allows for passing from a pointer to the variable being pointed to
+* Pointer dereferencing - usage of the indirection operator with a pointer that has been defined, in order to access the
+  value stored at the address it holds
+* Pointer constants - pointers reserved for arrays
+* Null pointer - a pointer that has been initialized to address 0 (or NULL)
+* Constant pointer - a pointer that cannot change the address it stores
+* Dynamically allocated memory - allotment of unused memory large enough to hold a variable of a specific data type using
+  the `new` operator
+* Heap - special memory cells of a program that are reserved for dynamic assignment during runtime
+* Exception - a mechanism for notifying a program that something has gone wrong with an operation that was being
+  executed and that its results are unreliable; by default, the response to an exception is forcing program termination
+* Dangling pointer - a pointer that points to a heap location that has been freed by `delete`
+* Memory leak - occurs when memory allocated with `new` is not freed with `delete`, making it unavailable for use until a
+  program's termination
+* 
+
+General
+=======
+~ Every variable is assigned a memory location whose address can be retrieved using the address operator &
+~ Current computer hardware can access data that resides at even addresses faster than data residing at odd addresses,
+  so most compilers allocate space in such a way that individual variables are always assigned even addresses
+~ C++ displays addresses in hexadecimal by default
+~ int* ptr: "ptr is a pointer to int" = "the variable that ptr points to has type int"
+~ Array names can be used as pointer constants, because an array name, with brackets and a subscript, represents the
+  starting address of the array; similarly, pointers can be used as array names, without the need for dereferencing
+~ Elements in an array can be accessed by subscript (e.g. array[i]) or by addition of its subscript to the array pointer
+  (e.g. *(arrayPtr + i))
+~ C++ performs no bounds checking with arrays, so it's possible to accidentally access a cell outside an array when
+  stepping through an array
+~ The only difference between array names and pointer variables is that the address of an array name cannot be changed
+~ Legal pointer arithmetic:
+  1) Increment/decrement operators
+  2) Integer addition or subtraction, including += & -=
+  3) subtraction between pointers
+~ Pointers may only be initialized with addresses of objects already defined
+~ Local pointer variables that have not been initialized do not hold valid addresses; use of these pointers will cause
+  runtime errors
+~ Convention for older versions of C++ has been to initialize pointers without a valid address to 0; many computers
+  keep OS data in address 0, inaccessible to user programs, so it remains a safe value for indicating an invalid
+  memory location
+~ Many header files, such as iostream, fstream, and cstdlib, define constant NULL to represent the pointer value 0;
+  therefore, pointer initialization can be rewritten if one of them is included--this is often preferred, since NULL
+  is more clearly recognized as an initialized pointer
+~ In C++11, empty braces at the end of a variable declaration initializes it to a null value (i.e. 0, 0.0, nullptr)
+~ Best practices:
+  1) Pointers should always be initialized or assigned a value close to where they are declared
+  2) A pointer no longer pointing a valid location should be assigned nullptr unless the pointer is going out of scope
+     or the program is about to terminate
+~ The relational operators may be used to compare pointers--equivalent to comparing their address values by magnitude
+~ Assigning a pointer to input without dereferencing can lead to potentially disastrous results
+~ When passing the address of a constant into a pointer variable, the variable must be defined as a pointer to a
+  constant
+~ Pointer to const: points to a constant, where the data pointed to cannot change but the pointer may change
+~ const pointer: pointer may not point to any other address
+~ Constant pointers must be initialized with a starting value
+~ Every call to `new` allocates storage from a special area of the program's memory called the heap; if a program
+  makes too many requests for dynamic memory, the heap will become depleted and additional calls to `new` will fail
+  (e.g. a bad_alloc exception is thrown)
+~ To free a dynamically allocated block of memory and return it to the heap, use the `delete` operator and pass it
+  the address of memory to be deallocated, then assign the pointer to nullptr--the only exception is cases where
+  the deleted pointer will clearly never be used again (e.g. the end of a program)
+~ Unless a pointer is going out of scope, best practice is to assign free store pointers to nullptr immediately after
+  `delete` has been called on it--this prevents problems that can arise from using `delete` on a pointer more than
+  once, because `delete` does nothing on a nullptr
+~ Dangling pointer prevention:
+  1) Assigning pointers to nullptr as soon as memory is freed
+  2) Verifying that a pointer is not nullptr before accessing its memory
+~ Memory leaks can be especially serious when they occur in loops--even more so in network-based programs that run
+  continuously for months or years
+*/
+
+void doubleValue(int*);
+void doubleValue(double*);
+void getFloat(float*);
+double sumArray(double*, int);
+void displayConstantArray(const int*, int);
+void displayNonconstantArray(int*, int);
+
+int main() {
+    float floatVariable;
+    float* floatPointer = &floatVariable;     // classic pointer declaration
+    float *anotherFloatPointer;               // alternative pointer declaration
+    float declaredFloat, *thirdFloatPointer;  // inline type & pointer declaration
+    int anInteger = 10, *thisPointer = &anInteger;  // inline type & pointer definition
+    int anotherInteger = 42, *thatPointer = &anotherInteger;
+    double quarters[4], *proportions = quarters;
+    char* zeroPointer = 0;      // traditional default pointer initialization
+    char* nullPointer = NULL;   // equivalent to a zero-initialized pointer
+
+    // Display memory address
+    cout << long(&floatVariable);
+
+    // Display float variable value via float pointer dereferencing
+    cout << long(*floatPointer);
+
+    // Pointer operations
+    thisPointer - thatPointer;  // pointer subtraction
+    thisPointer++;              // pointer increment
+    thatPointer--;              // pointer decrement
+    thisPointer += 2;           // integer addition
+    thatPointer -= 5;           // integer subtraction
+
+    // Null pointer
+    cout << "pointer(0) = pointer(NULL) = nullptr: " << ((zeroPointer == nullPointer && nullPointer == nullptr) ? "true" : "false") << endl;
+
+    // C++11 initialization to defaults
+    int defaultInteger{};   // 0
+    double defaultDouble{}; // 0.0
+    char* defaultPointer{}; // nullptr
+
+    // Passing pointers to functions
+
+    doubleValue(&anInteger);
+    doubleValue(&defaultDouble);
+    getFloat(anotherFloatPointer);  // passing pointer instead of variable reference
+
+    double doubles[] = {3.14, 4.37, 7.37, 42.0};  // already recognized as a pointer
+    cout << "Sum of array is " << sumArray(doubles, 4) << endl;
+
+    // Constant pointer parameters
+
+    const int integerArray[] = {1, 2, 3, 4, 5};
+    int anotherArray[] = {6, 7, 8, 9, 10};
+
+    displayConstantArray(integerArray, 5);
+    displayConstantArray(anotherArray, 5);
+    // displayNonconstantArray(integerArray, 5); -- illegal
+    displayNonconstantArray(anotherArray, 5);
+
+    // Constant pointers
+    int* const CONSTANT_POINTER = &anInteger; // pointer must always point to this address, but value at address may change
+    const int* const CONST_PTR_CONST_VALUE = &anotherInteger; // pointer must always point to this address, and value cannot be changed
+
+    // Dynamic memory allocation
+
+    int* freeStorePointer = nullptr;  // pointer initialization--this is not dynamic memory allocation
+    freeStorePointer = new int;       // the `new` operator allocates free store cell(s) for this variable
+    *freeStorePointer = 42;           // assignment of a value to keep in the allocated cell
+    freeStorePointer = new int[100];  // array created in free store--we can reassign int-type pointer to array name
+
+    // Returning memory to the heap
+    
+    delete [ ] freeStorePointer;  // no brackets for a non-array type
+    freeStorePointer = nullptr;
+
+    return 0;
+}
+
+void doubleValue(int* integer) {
+    *integer *= 2;
+}
+
+void doubleValue(double* decimal) {
+    *decimal *= 2;
+}
+
+void getFloat(float* number) {
+    cin >> *number;
+}
+
+double sumArray(double* array, int size) {
+    double sum = 0.0;
+    for (int i=0; i<size; i++) {
+        cout << "Adding " << array[i] << " to sum" << endl;  // array may still be indexed as a pointer
+        sum += *array++;  // steps through array without indexing
+    }
+    return sum;
+}
+
+void displayConstantArray(const int* integers, int size) {
+    for (int i=0; i<size; i++) {
+        cout << *(integers + i) << endl;
+    }
+    cout << endl;
+}
+
+void displayNonconstantArray(int* integers, int size) {
+    for (int i=0; i<size; i++) {
+        cout << *(integers + i) << endl;
+    }
+}
+
+void displayTrueConstantArray(const int* const integers, int size) {
+    for (int i=0; i<size; i++) {
+        cout << *(integers + i) << endl;
+    }
+    cout << endl;
+}
