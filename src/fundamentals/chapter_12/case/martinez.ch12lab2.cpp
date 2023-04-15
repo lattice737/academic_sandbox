56796055c42233424a0ed42a4eb1ec62b7574473
Nicholas Martinez <nedwardmartinez@gmail.com>
2023-04-14 19:29:47 -0500

    Finalize 1337 ch12 lab 2

--- src/fundamentals/chapter_12/case/martinez.ch12lab2.cpp
+++ src/fundamentals/chapter_12/case/martinez.ch12lab2.cpp
@@ -1,6 +1,6 @@
 //******************************************************************
 // Programmer: Nicholas Martinez
-// Completed : 04/13/2023
+// Completed : 04/14/2023
 // Status    : Completed
 //
 // This program accepts a user-defined C-string and performs case-
@@ -19,30 +19,45 @@ void flip(char*);
 
 int main() {
     const int N = 100;  // C-string character limit
-    char userEntry[N];  // User-defined C-string variable
+    char userEntry[N];  // User-defined C-string
+    int padding = 46;   // Whitespace padding
 
     // Take user input as C-string
 
     cout << "\nEnter text less than " << N << " characters: ";
     cin.getline(userEntry, N-1);
+    padding += sizeof(userEntry[0]) / sizeof(userEntry);
 
     // Display the C-string and invert, lower, and capitalize the C-string
 
-    cout << "\nYour string is: \"" << userEntry << "\"" << endl;
+    cout << setw(padding)
+         << left << "Your string is: "
+         << right << "\"" << userEntry << "\"" << endl;
+    
+    
     flip(userEntry);
-    cout << "Your string with inverted-case characters is: \"" << userEntry << "\"" << endl;
+    cout << setw(padding)
+         << left << "Your string with inverted-case characters is: "
+         << right << "\"" << userEntry << "\"" << endl;
+    
+
     lower(userEntry);
-    cout << "Your string lowercased is: \"" << userEntry << "\"" << endl;
+    cout << setw(padding)
+         << left << "Your string lowercased is: "
+         << right << "\"" << userEntry << "\"" << endl;
+    
     upper(userEntry);
-    cout << "Your string capitalized is: \"" << userEntry << "\"";
+    cout << setw(padding)
+         << left << "Your string capitalized is: "
+         << right << "\"" << userEntry << "\"";
 
     return 0;
 }
 
 // Capitalizes the characters of a passed C-string
 void upper(char* string) {
-    int i = 0;
-    while (string[i] != '\0' && isalpha(string[i])) {
+    int i = 0;  // char index accumulator
+    while (string[i] != '\0') {
         string[i] = toupper(string[i]);
         i++;
     }
@@ -50,8 +65,8 @@ void upper(char* string) {
 
 // Lowercases the characters of a passed C-string
 void lower(char* string) {
-    int i = 0;
-    while (string[i] != '\0' && isalpha(string[i])) {
+    int i = 0;  // char index accumulator
+    while (string[i] != '\0') {
         string[i] = tolower(string[i]);
         i++;
     }
@@ -59,8 +74,8 @@ void lower(char* string) {
 
 // Inverts the case of a passed C-string's characters
 void flip(char* string) {
-    int i = 0;
-    while (string[i] != '\0' && isalpha(string[i])) {
+    int i = 0;  // char index accumulator
+    while (string[i] != '\0') {
         string[i] = isupper(string[i]) ? tolower(string[i]) : toupper(string[i]);
         i++;
     }
