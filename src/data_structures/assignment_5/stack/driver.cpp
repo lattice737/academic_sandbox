6b6903902cd5e0c09ead69d6caf304f6feca9c96
Nicholas Martinez <nedwardmartinez@gmail.com>
2023-04-20 14:37:19 -0500

    Implement stack examples for 2436 ch6 reading

---
+++ src/data_structures/assignment_5/stack/driver.cpp
@@ -0,0 +1,149 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+bool bracesBalanced(string);
+bool stringInLanguage(string);
+void convertInfixExpression(char*);
+
+int main() {
+
+    cout << "{a{b}c} -> " << (bracesBalanced ? "true" : "false") << endl
+         << "{a{bc}  -> " << (bracesBalanced ? "true" : "false") << endl
+         << "{ab}c}  -> " << (bracesBalanced ? "true" : "false") << endl;
+    
+    cout << "a$a   -> " << (stringInLanguage ? "true" : "false") << endl
+         << "ab$ab -> " << (stringInLanguage ? "true" : "false") << endl
+         << "ab$a  -> " << (stringInLanguage ? "true" : "false") << endl
+         << "ab$ba -> " << (stringInLanguage ? "true" : "false") << endl;
+
+    cout << "7 3 - 2 + -> " << calculatePostfixExpression("7 3 - 2 +");
+
+    // cout << " a / b * c -> " << convertPostfixExpression("a / b * c");
+
+    return 0;
+}
+
+bool bracesBalanced(string chars) {
+    Stack aStack = Stack();
+    bool balanced = true;
+    char thisChar;
+    int i = 0;  // Trackes character position in string
+
+    while (balanced && i < chars.length()) {
+        thisChar = chars[i];
+        i++;
+
+        if (thisChar == '{')
+            aStack.push('{');
+        else if (thisChar == '}') {
+            if (!aStack.isEmpty())
+                aStack.pop();
+            else
+                balanced = false;
+        }
+    }
+
+    return balanced && aStack.isEmpty() ? true : false;
+}
+
+bool stringInLanguage(string someString) {
+    Stack aStack = Stack();
+    char thisChar = someString[0],
+         stackTop;
+    bool inLanguage = true;
+    int i = 0;
+
+    while (thisChar != '$') {
+        aStack.push(thisChar);
+        i++;
+        thisChar = someString[i];
+    }
+
+    i++;
+
+    while (inLanguage && i < someString.length()) {
+        if (!aStack.isEmpty()) {
+            stackTop = aStack.peek();
+            aStack.pop();
+            thisChar = someString[i];
+            if (stackTop == thisChar)
+                i++;
+            else
+                inLanguage == false;
+        } else
+            inLanguage = false;
+    }
+
+    return inLanguage && aStack.isEmpty() ? true : false;
+}
+
+double calculatePostfixExpression(string expression) {
+    Stack expressionStack = Stack();
+    int operands[] = { '+', '-', '*', '/'};
+    double operand2, operand1, result;
+    
+    for (char thisChar : expression) {
+        if (operands.contains(thisChar))    // check if operand array contains char
+            expressionStack.push(thisChar);
+        else {
+            operand2 = expressionStack.peek(); // get top item
+            expressionStack.pop();
+            operand1 = expressionStack.peek();
+            expressionStack.pop();
+
+            switch (thisChar) {
+                case '+': result = static_cast<double>(operand1) + static_cast<double>(operand2);
+                case '-': result = static_cast<double>(operand1) - static_cast<double>(operand2);
+                case '*': result = static_cast<double>(operand1) * static_cast<double>(operand2);
+                case '/': result = static_cast<double>(operand1) / static_cast<double>(operand2);
+            }
+
+            expressionStack.push(result);
+        }
+    }
+
+    return expressionStack.peek();
+}
+
+void convertInfixExpression(char* expression) {
+    Stack operatorStack = Stack(); 
+    char operators[] = {'+', '-', '*', '/'},
+         thisChar;
+    int i = 0;
+
+    while (expression[i] != '\0') {
+        thisChar = expression[i];
+        switch (thisChar) {
+            case "operand":
+                // TODO append operand to end of postfixExpression
+                break;
+            case '(':
+                operatorStack.push(thisChar);
+                break;
+            case operators.contains(thisChar):
+                while (!operatorStack.isEmpty() && operatorStack.peek() != '!' && precedence(thisChar) <= precedence(operatorStack.peek())) {
+                    // TODO append operatorStack.peek() to the end of postfixExpression
+                    operatorStack.pop();
+                }
+                operatorStack.push(thisChar);
+                break;
+            case ')':
+                while (operatorStack.peek() != '(') {
+                    // TODO append operatorStack.peek() to the end of postfixExpression
+                    operatorStack.pop();
+                }
+                operatorStack.pop();
+                break;
+        }
+        i++;
+    }
+
+    while (!operatorStack.isEmpty()) {
+        // TODO append operatorStack.peek() to the end of postfixExpression
+        operatorStack.pop();
+    }
+
+    // TODO return postfixExpression
+}
--- src/data_structures/reading/chapter_6.cpp
+++ src/data_structures/reading/chapter_6.cpp
@@ -1,10 +1,13 @@
-#include<iostream>
-
 using namespace std;
 
 /*
 Vocabulary
 ==========
+* Last in, first out (LIFO) - characterization of a container, usually a stack, where the last item inserted is also the
+  first one removed
+* First in, first out (FIFO) - characterization of a container, usually a queue, where the first item inserted is also the
+  first one removed
+* Axiom - a mathematical rule implemented to be a true statement for an ADT's operation
 * 
 
 General
@@ -16,6 +19,12 @@ General
 ~ Data can be organized either by position or by value
 ~ The specification of an abstract data type that you can use to solve a particular problem can emergy during the design
   of the problem's solution
+~ In the spirit of fail-safe programming, a function that implements the text's balanced-braces algorithm should check
+  a stack's push() method return value
+~ Knowing how to convert an expression from infix to postfix notation not only lead to an algorithm to evaluate infix
+  expressions, but also will give you some insight into the compilation process
+~ Algorithms that evaluate an infix expression or transform one to postfix form must determine which operands to apply
+  to a given operator, in order to allow for precedence and left-to-right association where parentheses are omitted
 ~ 
 */
 
