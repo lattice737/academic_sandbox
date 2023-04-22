a51ca41f43524cbf215b845ed6c3876054437136
Nicholas Martinez <nedwardmartinez@gmail.com>
2023-04-22 00:16:18 -0500

    2436 ch8 reading notes + list interface

---
+++ src/data_structures/reading/chapter_8.cpp
@@ -0,0 +1,29 @@
+using namespace std;
+
+/*
+Vocabulary
+==========
+* List - an ordered container of items with the same type and positions referenced by number
+* Predecessor - the item positioned before a given list item
+* Successor - the item positioned after a given list item
+* Head - the first item of a list--has no predecessor
+* Tail - the last item of a list--has no successor
+* Insertion sort - Sorting items of a container as they are inserted
+
+General
+=======
+~ The first entry in a list has position 1--not 0, as in an array
+~ List operation categories:
+  1) insert - adds data to a collection
+  2) remove - removes data from a collection
+  3) questions - i.e. isEmpty(), getLength(), getEntry()
+  * replace removes an item and then adds a different item
+~ A set of axioms, preconditions, and postconditions are necessary to define the behavior of an ADT's operations
+  completely
+~ Displaying list items and replacing items do not qualify as ADT list operations, so they should be implemented
+  as client functions
+*/
+
+int main() {
+    return 0;
+}
